e96859dee0da10d4dc02bc0dc4c7db959fd70f33
QuadrixYT <andrehillay@gmail.com>
2024-03-16 11:12:08 +0100

    changes

--- dev/products.php
+++ dev/products.php
@@ -9,6 +9,7 @@ $result = $db->query($sql);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,53 +22,56 @@ $result = $db->query($sql);
             background-color: #1e1e1e;
             color: #fff;
         }
+
         .mt-5 {
-            margin-top: 3rem; /* Anpassung der Margin-Top-Eigenschaft */
+            margin-top: 3rem;
+            /* Anpassung der Margin-Top-Eigenschaft */
         }
     </style>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="../index.html">Serverhosting-Main | Startseite</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-            <?php if(isset($_SESSION['loggedin'])) { ?>
-            <li class="nav-item">
-              <a class="nav-link" href="logout.php">Logout</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="cart.php"><i class="fas fa-shopping-cart"></i></a>
-            </li>
-            <?php } else { ?>
-            <li class="nav-item">
-              <a class="nav-link" href="login.php">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="register.php">Register</a>
-            </li>
-            <?php } ?>
-          </ul>
+        <div class="container">
+            <a class="navbar-brand" href="../index.html">Serverhosting-Main | Startseite</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <?php if (isset ($_SESSION['loggedin'])) { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="cart.php"><i class="fas fa-shopping-cart"></i></a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
         </div>
-      </div>
     </nav>
-    <div class="container mt-5">
+    <div class="container mt-5" style="margin-top: 5rem;">
         <h2 class="text-center mb-4">Produkte</h2>
         <div class="row">
             <?php
@@ -77,12 +81,23 @@ $result = $db->query($sql);
                     <div class="col-md-4">
                         <div class="card mb-4">
                             <div class="card-body">
-                                <h5 class="card-title"><?php echo $row['name']; ?></h5>
-                                <p class="card-text">RAM: <?php echo $row['ram']; ?> MB</p>
-                                <p class="card-text">Cores: <?php echo $row['cores']; ?></p>
-                                <p class="card-text">IPs: <?php echo $row['ips']; ?></p>
-                                <p class="card-text">Preis: $<?php echo $row['price']; ?></p>
-                                <a href="add_to_cart.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">In den Warenkorb</a>
+                                <h5 class="card-title">
+                                    <?php echo $row['name']; ?>
+                                </h5>
+                                <p class="card-text">RAM:
+                                    <?php echo $row['ram']; ?> MB
+                                </p>
+                                <p class="card-text">Cores:
+                                    <?php echo $row['cores']; ?>
+                                </p>
+                                <p class="card-text">IPs:
+                                    <?php echo $row['ips']; ?>
+                                </p>
+                                <p class="card-text">Preis: $
+                                    <?php echo $row['price']; ?>
+                                </p>
+                                <a href="add_to_cart.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">In den
+                                    Warenkorb</a>
                             </div>
                         </div>
                     </div>
@@ -99,4 +114,5 @@ $result = $db->query($sql);
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
