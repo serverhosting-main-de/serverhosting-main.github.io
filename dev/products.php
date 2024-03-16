d33f6a1eec7b567dedc2fc275ac29716056ceeeb
QuadrixYT <andrehillay@gmail.com>
2024-03-16 11:03:21 +0100

    test remove index

--- dev/products.php
+++ dev/products.php
@@ -14,7 +14,6 @@ $result = $db->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produkte</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../assets/style/index.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <style>
         /* Dark mode */
